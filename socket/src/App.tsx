e8e47e6eca2b98ab90f8454609827467a6fb7a87
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-08-18 23:17:46 +0900

    socket - 채팅창 생성

--- socket/src/App.tsx
+++ socket/src/App.tsx
@@ -7,13 +7,28 @@ const userList = ['뽀야미', '쭈니', '잭슨'];
 function App() {
   const [user, setUser] = useState('');
 
+  const openChat = (value: string) => {
+    fetch('http://52.78.66.72:8081/chat/room', {
+      method: 'POST',
+      body: `name=${value}`,
+      headers: {
+        'Content-Type': `application/x-www-form-urlencoded`,
+      },
+    }).then((res) => res.json());
+  };
+
   return (
     <main>
       <section className='users'>
         {userList.map((name) => (
           <div key={name} className='user'>
             <div>{name}</div>
-            <button type='button' onClick={() => setUser(name)}>
+            <button
+              type='button'
+              onClick={() => {
+                setUser(name);
+                openChat(name);
+              }}>
               대화하기
             </button>
           </div>
