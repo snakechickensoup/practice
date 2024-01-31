0c9951e01df8365d4fef6996c56ee046f2350200
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-01-31 15:02:55 +0900

    feat edit, delete todo

--- todos/src/components/TodoItem.tsx
+++ todos/src/components/TodoItem.tsx
@@ -1,16 +1,58 @@
+import { useContext, useState } from 'react';
+import { TodosContext } from '../store';
+
 interface Props {
   title: string;
   id: number;
 }
 
 const TodoItem = (props: Props) => {
-  const { title } = props;
+  const { id, title } = props;
+  const { editTodo, delTodo } = useContext(TodosContext);
+  const [edit, setEdit] = useState<boolean>(false);
+  const [titleValue, setTitleValue] = useState<string>(title);
+
+  const handleEdit = () => {
+    setEdit((prev) => !prev);
+    editTodo(id, titleValue);
+  };
+
   return (
-    <li>
-      <b>{title}</b>
-      <button type='button'>📝</button>
-      <button type='button'>🗑️</button>
-    </li>
+    <>
+      <li className='todo-item'>
+        {edit ? (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleEdit();
+            }}>
+            <input
+              type='text'
+              onChange={(e) => setTitleValue(e.target.value)}
+              value={titleValue}></input>
+          </form>
+        ) : (
+          <b>{title}</b>
+        )}
+        <div>
+          <button
+            type='button'
+            onClick={() => {
+              if (edit) {
+                handleEdit();
+              } else {
+                setEdit((prev) => !prev);
+              }
+            }}>
+            📝
+          </button>
+          <button type='button' onClick={() => delTodo(id)}>
+            🗑️
+          </button>
+        </div>
+      </li>
+      <hr />
+    </>
   );
 };
 
--- todos/src/components/TodoList.tsx
+++ todos/src/components/TodoList.tsx
@@ -5,7 +5,7 @@ import { TodosContext } from '../store';
 const TodoList = () => {
   const { items } = useContext(TodosContext);
   return (
-    <ul>
+    <ul className='todo-list'>
       {items.map((todo) => (
         <TodoItem {...todo} key={todo.id} />
       ))}
