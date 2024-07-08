39385ece14699a2f5e8b0f03e9f2f9df27e67c0e
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-07-08 22:28:57 +0900

    test switch component

--- design-system-test/src/App.tsx
+++ design-system-test/src/App.tsx
@@ -1,9 +1,11 @@
+import Switch from './component/switch';
 import Tags from './component/tags';
 
 function App() {
 	return (
 		<main className='main'>
 			<Tags />
+			<Switch />
 		</main>
 	);
 }
---
+++ design-system-test/src/component/switch.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { Switch } from 'snow-white-ui';
+
+const SwitchComponent = () => {
+	const [checked, setchecked] = useState(false);
+
+	const handleCheck = () => setchecked((prev) => !prev);
+	return (
+		<article>
+			<h2>Switch Component</h2>
+			<div className='switch-area'>
+				<Switch size='sm' checked={checked} onChange={handleCheck} />
+				<Switch size='md' checked={checked} onChange={handleCheck} />
+				<Switch size='lg' checked={checked} onChange={handleCheck} />
+			</div>
+		</article>
+	);
+};
+
+export default SwitchComponent;
