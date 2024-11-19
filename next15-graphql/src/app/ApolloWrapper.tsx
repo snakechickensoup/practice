0f74ed31cf2cebbcccfcdae512917320e287a2ed
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2024-11-19 10:49:27 +0900

    feat: jest, graphql 설치

---
+++ next15-graphql/src/app/ApolloWrapper.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/experimental-nextjs-app-support";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "https://spacex-production.up.railway.app/",
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
